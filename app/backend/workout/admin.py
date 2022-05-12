c75fd7cfb25bc1a89af00b4490fe97a5cfd7fd47
junwoo <top7578@naver.com>
2022-05-12 10:00:04 +0900

    [FEAT]WrongPoses테이블admin 추가

--- app/backend/workout/admin.py
+++ app/backend/workout/admin.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import WorkoutInfo, DayHistoryWorkout, DayHistoryExtraWorkout
+from .models import DayHistoryExtraWorkoutWrongPoses, DayHistoryWorkoutWrongPoses, WorkoutInfo, DayHistoryWorkout, DayHistoryExtraWorkout
 
 admin.site.register(WorkoutInfo)
 admin.site.register(DayHistoryWorkout)
 admin.site.register(DayHistoryExtraWorkout)
+admin.site.register(DayHistoryWorkoutWrongPoses)
+admin.site.register(DayHistoryExtraWorkoutWrongPoses)
